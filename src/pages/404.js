14d1063ed9c802a0b749aecadf8b28d5f531ff19
Vivek Gawande <vivek040997@gmail.com>
2019-06-26 20:19:24 +0530

    Fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { Layout } from '../components/layout'
-import Head from "../components/head"
+import React from "react";
+import Head from "../components/head";
+import Layout from "../components/layout";
+
+import { Link } from "gatsby";
 
 const NotFound = () => {
     return (
         <Layout>
             <Head title="404" />
-            
+
             <h1>Page not found!</h1>
-            <p><Link to="/">Home</Link></p>
+            <p><Link to="/">Go Home</Link></p>
         </Layout>
     )
 }
